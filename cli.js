d6190446660d0cae641f6b5c8298ee969068904b
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2018-10-24 17:21:47 +0200

    implemented deploy exploded

--- cli.js
+++ cli.js
@@ -1,15 +1,44 @@
 #!/usr/bin/env node
+const settings = require("settings-store")
 
-console.log("TXM Command Line Interface v" + process.env.npm_package_version);
-
-process.argv.forEach(arg => console.log(arg));
-
-if (process.argv.length<3) {
+function usage() {
+    console.log("TXM Command Line Interface v" + process.env.npm_package_version);
+    console.log();
     console.log("Usage:  txm <cmd>");
     console.log();
     console.log("with <cmd> being one of");
     console.log();
-    console.log("       srv:     manage your servers");
-    console.log("       repo:    manage your sandboxes");
-    console.log("       deploy:  deploy and explode from current sandbox to current server");
+    console.log("       server:     manage your servers");
+    console.log("       sandbox:    manage your sandboxes");
+    console.log("       deploy:     deploy and explode from current sandbox to current server");
+    console.log();
+    let defaults = global.settings.value("defaults");
+    if (defaults) console.log("Defaults: " + JSON.stringify(defaults, null, 2));
+    process.exit();
+}
+
+// load settings:
+settings.init({
+    appName:       "txm-cli", //required,
+    publisherName: "Diebold Nixdorf Inc.", //optional
+    reverseDNS:    "com.dieboldnixdorf.txm" //required for macOS
+});
+
+global.settings = settings;
+
+if (process.argv.length<3) usage();
+
+// now start the actual sub command program:
+let cmd = process.argv[2];
+
+callCli(cmd);
+
+async function callCli(cmd) {
+    try {
+        let cli = require("./cli/" + cmd);
+        await cli.invoke(process.argv.slice(3));
+    } catch (err) {
+        console.log(err);
+    }
+    process.exit();
 }
---
+++ cli/sandbox.js
@@ -0,0 +1,59 @@
+function usage() {
+    console.log("Usage:  txm sandbox <cmd>");
+    console.log();
+    console.log("with <cmd> being one of");
+    console.log();
+    console.log("       list:                list configured sandboxes");
+    console.log("       set <name> <path>:   set or update a sandbox");
+    console.log("       del <name>:          delete a sandbox");
+    console.log("       default <name>:      sets the current default sandbox");
+    
+    process.exit();
+}
+
+function invoke(args) {
+
+    if (!args.length) usage();
+
+    let cmd = args[0];
+    if (cmd === "list") list();
+    else if (cmd === "set") set(args[1], args[2]);
+    else if (cmd === "del") del(args[1]);
+    else if (cmd === "default") def(args[1]);
+    else {
+        console.log("Unknown command: " + cmd);
+        usage();
+    }
+}
+
+function list() {
+    let sandboxes = global.settings.value("sandboxes");
+    if (!sandboxes) {
+        console.log("No sandboxes configured");
+        return;
+    } 
+    let d = global.settings.value("defaults.sandbox");
+    Object.keys(sandboxes).forEach(key => {
+        console.log((sandboxes[key].name==d ? "* " : "  ") + "[" + sandboxes[key].name + "]:\t" + sandboxes[key].path);
+    })
+}
+
+function set(name, path) {
+    if (!name || !path) usage();
+    global.settings.setValue("sandboxes."+name, {name:name, path:path});
+    def(name); // make default
+}
+
+function del(name) {
+    if (!name) usage();
+    global.settings.delete("sandboxes." + name);
+    list();
+}
+
+function def(name) {
+    if (!name) usage();
+    global.settings.setValue("defaults.sandbox", name);
+    list();
+}
+
+module.exports.invoke = invoke;
