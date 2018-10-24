b2bcdaaa3febf79a8e5ada25ca810fa7fe92e168
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2018-10-24 18:07:23 +0200

    added version command

--- cli.js
+++ cli.js
@@ -1,19 +1,20 @@
 #!/usr/bin/env node
 const settings = require("settings-store")
 
+const KNOWN_COMMANDS = ["version","server","sandbox","deploy"];
+
 function usage() {
-    console.log("TXM Command Line Interface v" + require('./package.json').version);
-    console.log();
     console.log("Usage:  txm <cmd>");
     console.log();
     console.log("with <cmd> being one of");
     console.log();
+    console.log("       version:    display version info");
     console.log("       server:     manage your servers");
     console.log("       sandbox:    manage your sandboxes");
     console.log("       deploy:     deploy and explode from current sandbox to current server");
     console.log();
     let defaults = global.settings.value("defaults");
-    if (defaults) console.log("Defaults: " + JSON.stringify(defaults, null, 2));
+    if (defaults) console.log("Current defaults:\n" + JSON.stringify(defaults, null, 2));
     process.exit();
 }
 
@@ -30,6 +31,11 @@ if (process.argv.length<3) usage();
 
 // now start the actual sub command program:
 let cmd = process.argv[2];
+
+if (!KNOWN_COMMANDS.includes(cmd)) {
+    console.log("Unknown command: " + cmd);
+    usage();
+}
 
 callCli(cmd);
 
