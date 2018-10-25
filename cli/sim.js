0b8c30e9ad68d343716111d9e796c53e64b8c522
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2018-10-25 12:00:34 +0200

    added "txm sim"

---
+++ cli/sim.js
@@ -0,0 +1,15 @@
+const util = require('../utils/util');
+
+async function invoke(args) {
+    let sbox = global.settings.value("sandboxes." + global.settings.value("defaults.sandbox"));
+    let execpath = sbox.path + "\\Projects\\SI\\Tools\\PBMSimulatorGUI\\SimulationFiles";
+    let executable = execpath + "\\SIPbmSimulatorConfigurator.exe";
+    let chameleonpath = sbox.path + "\\runtime\\chameleon";
+    let gwpath = sbox.path + "\\runtime\\fi-pbmc-fcgateway";
+    let simpath = sbox.path + "\\runtime\\pbm-simclient";
+    let exeargs = [chameleonpath, gwpath, simpath, sbox.path, "8088", "false"];
+    console.log(exeargs);
+    util.spawnDetached(executable, exeargs, execpath);
+}
+
+module.exports.invoke = invoke;
