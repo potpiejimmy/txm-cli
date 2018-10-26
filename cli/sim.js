1e0bac2170970c2db20abc5e025637fdf6a41fa2
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2018-10-26 12:52:09 +0200

    pass txm server port to sim gui

--- cli/sim.js
+++ cli/sim.js
@@ -2,8 +2,9 @@ const util = require('../utils/util');
 
 async function invoke(args) {
     let sbox = global.settings.value("sandboxes." + global.settings.value("defaults.sandbox"));
-    if (!sbox) {
-        console.log("Please configure default sandbox first.");
+    let servers = global.settings.value("servers");
+    if (!sbox || !servers) {
+        console.log("Please configure default sandbox and server(s) first.");
         return;
     }
     let execpath = sbox.path + "\\Projects\\SI\\Tools\\PBMSimulatorGUI\\SimulationFiles";
@@ -11,9 +12,18 @@ async function invoke(args) {
     let chameleonpath = sbox.path + "\\runtime\\chameleon";
     let gwpath = sbox.path + "\\runtime\\fi-pbmc-fcgateway";
     let simpath = sbox.path + "\\runtime\\pbm-simclient";
-    let exeargs = [chameleonpath, gwpath, simpath, sbox.path, "8088", "false"];
+    let exeargs = [chameleonpath, gwpath, simpath, sbox.path, determineServerPort(servers), "false"];
     console.log(exeargs);
     util.spawnDetached(executable, exeargs, execpath);
+}
+
+function determineServerPort(servers) {
+    let d = global.settings.value("defaults.server");
+    for (let key of Object.keys(servers)) {
+        let server = servers[key];
+        if (server.name.startsWith(d) && server.type == 'txm') return server.port;
+    }
+    return 8080;
 }
 
 module.exports.invoke = invoke;
