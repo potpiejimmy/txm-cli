00e5104c98d3d0d3f4b7ac1586f9a3d8d6d52905
potpiejimmy <t.liese@outlook.com>
2019-02-18 20:05:55 +0100

    fixed upgrading from official npm

--- cli/update.js
+++ cli/update.js
@@ -2,7 +2,7 @@ const util = require('../utils/util');
 
 async function invoke(args) {
     var win = process.platform === "win32";
-    await util.spawn(win ? "npm.cmd" : "npm",["update","-g","txm-cli"], process.cwd());
+    await util.spawn(win ? "npm.cmd" : "npm",["i","-g","txm-cli"], process.cwd());
 }
 
 module.exports.invoke = invoke;
