b3cc76890c339979f53803cfba573f408dbf66c3
Liese, Thorsten <thorsten.liese@dieboldnixdorf.com>
2019-06-21 09:07:13 +0200

    simplified hotfix command, target the default servers

--- cli/hotfix.js
+++ cli/hotfix.js
@@ -5,46 +5,51 @@ const server = require('./server');
 const deploy = require('./deploy');
 
 function usage() {
-    console.log("Usage:  tm hotfix <server> <module>");
+    console.log("Usage:  tm hotfix <module>");
     console.log();
-    console.log("  Stops the given server(s), explodes the given module in the deployment folder,");
+    console.log("  Stops the targeted server(s), explodes the given module in the deployment folders,");
     console.log("  applies the IDE-compiled classes from <module>/out/production/classes on top of");
     console.log("  it, restarts the server(s).");
     console.log();
-    console.log("  <server> needs to denote a server name/prefix or index as per 'tm server list'.");
-    console.log("  <module> specifies the module folder name which is also the JAR file base name.");
+    console.log("  <module> specifies the Gradle subproject name, i.e. the JAR file base name.");
 
     process.exit();
 }
 
 async function invoke(args) {
     
-    if (args.length != 2) usage();
+    if (args.length != 1) usage();
 
-    let servername = args[0];
-    let modulename = args[1];
+    let modulename = args[0];
+    let defserver = global.settings.value("defaults.server");
 
     let sbox = global.settings.value("sandboxes." + global.settings.value("defaults.sandbox"));
 
-    await server.invoke(["stop",servername]);
+    await server.invoke(["stop", defserver]);
 
-    servername = server.indexToNameIfIndex(servername); // allow index
+    console.log("Hotfixing the servers " + defserver + "*");
+    await util.asyncPause(5000);
     let servers = global.settings.value("servers");
     for (let server of Object.values(servers)) {
-        if (server.name.startsWith(servername)) {
+        if (server.name.startsWith(defserver)) {
             console.log("-----");
             let modulepath = deploy.getDeploymentPath(server) + '/' + modulename + '.jar';
-            if (fs.lstatSync(modulepath).isFile()) {
-                util.unjar(modulepath); // unjar if it exists as a file
-            }
-            if (fs.lstatSync(modulepath).isDirectory()) {
-                console.log("Hotfixing " + modulepath);
-                await util.copytree(sbox.path + '/' + modulename + '/out/production/classes/', modulepath);
+            if (fs.existsSync(modulepath)) {
+                if (fs.lstatSync(modulepath).isFile()) {
+                    util.unjar(modulepath); // unjar if it exists as a file
+                }
+                if (fs.lstatSync(modulepath).isDirectory()) {
+                    console.log("Hotfixing " + modulepath);
+                    await util.copytree(sbox.path + '/' + modulename + '/out/production/classes/', modulepath);
+                }
+            } else {
+                console.log("Module '" + modulename + "' does not exist on server '" + server.name + "'");
             }
         }
     };
+    console.log("-----");
 
-    await server.invoke(["start",servername]);
+    await server.invoke(["start", defserver]);
 }
 
 module.exports.invoke = invoke;
