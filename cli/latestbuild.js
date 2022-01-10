0c63a3bd8fff95fd6ce7f4907273b70b92425c9b
Jakub <onemoreplayscz@gmail.com>
2022-01-10 17:40:15 +0100

    Added tm version parameter, added versions fetching, added stable version search

--- cli/latestbuild.js
+++ cli/latestbuild.js
@@ -3,30 +3,31 @@ const util = require('../utils/util');
 const xml2js = require('xml2js');
 
 function usage() {
-    console.log("Usage:  tm latestbuild <branch> <dependency>");
+    console.log("Usage:  tm latestbuild <br> <dep> <ver>");
     console.log();
     console.log("  Tells you latest product build by your chosen release branch \w changelogs.");
     console.log();
-    console.log("  <branch> stands for the selected release branch. release or snapshot");
-    console.log("  <dependency> here you can specify your requested dependency. Keep this form: groupId/artifactId")
+    console.log("  <br> stands for the selected release branch. public (stable) or snapshot (development)");
+    console.log("  <dep> here you can specify your requested dependency. Keep this form: groupId/artifactId")
+    console.log("  <ver> means your TM version. For example: 2.3.0, 3.0.0, 3.1.0 etc.")
 
     process.exit();
 }
 
 async function invoke(args) {
 
-    if (args.length !== 2) usage();
+    if (args.length !== 3) usage();
     return await getData(args);
 }
 
 async function getData(args){
     let url = await createUrl(args[0], args[1])
     let authToken = util.getBase64(await util.getAuthKey('auth-nexusde'))
-    let latestVersion = await fetchLatestVersion(url, authToken);
+    let latestVersion = await fetchLatestVersion(url, authToken, args[2]);
     console.log(latestVersion);
 }
 
-async function fetchLatestVersion(url, authToken){
+async function fetchLatestVersion(url, authToken, tmVer){
     if (!authToken) return;
     return fetch(url + "/maven-metadata.xml", {
         headers: { Accept: "application/xml", Authorization: "Basic: " + authToken }
@@ -35,13 +36,28 @@ async function fetchLatestVersion(url, authToken){
         return result.text();
     }).then(textXml => {
         xml2js.parseString(textXml, function (err, res) {
-            console.log(res.metadata.versioning[0]['versions']);
+            let versions = res.metadata.versioning[0]['versions'];
+            for(let x = (versions.length-1); x <= 0; x--){
+                if(isCorrectVersion(versions[x], tmVer)){
+                    if(url.includes("public")){
+                        if((new RegExp(".+\-Build\.\d{2}")).test(versions[x])){
+                            return versions[x];
+                        }
+                    }else{
+                        //snapshot search. Here it's going to be a bit complicated.
+                    }
+                }
+            }
         })
     })
 }
 
 function createUrl(branch, dependency){
     return "https://nexusde.dieboldnixdorf.com/content/repositories/" + chooseBranch(branch) + "/com/dieboldnixdorf/txm/" + dependency;
+}
+
+function isCorrectVersion(version, tmver){
+    return version.includes(tmver);
 }
 
 function chooseBranch(branch){
