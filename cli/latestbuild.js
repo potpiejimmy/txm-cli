774d776dd3e68cdfdca077070c7ea5e731edf179
jakub.kaspar <npm@liese.space>
2022-01-11 07:46:20 +0000

    Working stable version search

--- cli/latestbuild.js
+++ cli/latestbuild.js
@@ -23,8 +23,8 @@ async function invoke(args) {
 async function getData(args){
     let url = await createUrl(args[0], args[1])
     let authToken = util.getBase64(await util.getAuthKey('auth-nexusde'))
-    let latestVersion = await fetchLatestVersion(url, authToken, args[2]);
-    console.log(latestVersion);
+    let latest = await fetchLatestVersion(url, authToken, args[2]);
+    console.log(latest);
 }
 
 async function fetchLatestVersion(url, authToken, tmVer){
@@ -36,20 +36,21 @@ async function fetchLatestVersion(url, authToken, tmVer){
         return result.text();
     }).then(textXml => {
         xml2js.parseString(textXml, function (err, res) {
-            let versions = res.metadata.versioning[0]['versions'];
-            for(let x = (versions.length-1); x <= 0; x--){
+            let versions = res.metadata.versioning[0]['versions'][0]['version'];
+            for(let x = (versions.length-1); x >= 0; x--){
                 if(isCorrectVersion(versions[x], tmVer)){
-                    if(url.includes("public")){
-                        if((new RegExp(".+\-Build\.\d+$")).test(versions[x])){
-                            return versions[x];
-                        }
+                    if(versions[x].match(/.+-Build\.\d+$/)){
+                        textXml = versions[x];
+                        break;
                     }else{
                         //snapshot search. Here it's going to be a bit complicated.
                     }
                 }
             }
         })
+        return textXml;
     })
+        .catch((err) => console.log(err))
 }
 
 function createUrl(branch, dependency){
