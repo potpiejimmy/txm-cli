2a7923a5ab8c8038be0a52f381d0e501b88ad0d7
jakub.kaspar <jakub.kaspar@dieboldnixdorf.com>
2022-07-15 13:45:58 +0100

    Update help in main cli.js

--- cli.js
+++ cli.js
@@ -26,9 +26,10 @@ function usage() {
     console.log("       sim            configure and run the PBM simulator GUI.");
     console.log("       ropssim [ui]   configure and run ROPS gateway and ROPS cmd client,");
     console.log("                      specify option 'ui' to start the GUI version.");
-    console.log("       lastbn [<r> [d]] display last build number (and copy to clipboard),");
+    console.log("       lastbn <r> <b> [d] display last build number (and copy to clipboard),");
     console.log("                      with <r> being the desired version (e.g. '19.0.00'),");
     console.log("                      if <r> is omitted, the current sandbox version is used,");
+    console.log("                      <b> is required branch.");
     console.log("                      specify option 'd' to download the artifact from Nexus.");
     console.log("       latestbuild <branch> <dep> <ver> displays latest product build number")
     console.log("                      more info in command usage ;)")
