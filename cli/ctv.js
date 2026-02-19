027abe9ded7fa63e2822da72a2279ef1c3d3f62d
Helmut Heinreichsberger <h.heinreichsberger@outlook.de>
2026-02-19 10:56:08 +0100

    correct path and call of the commtraceviewer.

--- cli/ctv.js
+++ cli/ctv.js
@@ -1,10 +1,28 @@
-import * as util from '../utils/util.js';
+import process from "node:process";
+import {exec} from "../utils/util.js";
+import fs from "node:fs"
 
 export async function invoke(args) {
-    let sbox = global.settings.value("sandboxes." + global.settings.value("defaults.sandbox"));
+    let win = process.platform === "win32";
+
+    let sandbox = globalThis.settings.value("sandboxes." + globalThis.settings.value("defaults.sandbox"));
 	
-	let execpath = sbox.path + "\\runtime\\commtraceviewer";
-    let executable = execpath + "\\ctv.exe";
-	let exeargs = [];
-    util.spawnDetached(executable, exeargs, execpath);
+	let binDir = sandbox.path + "/runtime/CommTraceViewer/bin";
+    let execPath = binDir + '/commtraceviewer.';
+    if (win) {
+
+        if (fs.existsSync(execPath + 'bat')) {
+            execPath = execPath + 'bat';
+        } else  if (fs.existsSync(execPath + 'cmd')) {
+            execPath = execPath + 'cmd';
+        } else {
+            console.log("Cant determine execute script")
+            return;
+        }
+
+        await exec(execPath, binDir)
+    } else {
+        execPath = execPath + 'sh';
+        await exec(execPath, binDir);
+    }
 }
